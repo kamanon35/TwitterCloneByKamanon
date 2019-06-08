59ff1692aacd8a83c234380238f36867140464a9
kamanon35 <k.amano.javajava@gmail.com>
2019-06-09 08:08:27 +0900

    プロジェクト作成した段階（Javaバージョン12へ変更に伴うエラー解消済）

---
+++ TwitterCloneByKamanon/src/main/java/com/kamanon/TwitterCloneByKamanonApplication.java
@@ -0,0 +1,13 @@
+package com.kamanon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterCloneByKamanonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterCloneByKamanonApplication.class, args);
+	}
+
+}
---
+++ TwitterCloneByKamanon/src/test/java/com/kamanon/TwitterCloneByKamanonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamanon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneByKamanonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
